6543491cc6aae867b6db00562e2c9a7a87a58a2c
krishnashrimali88 <krishnashrimali008@gmail.com>
2026-06-04 11:30:36 +0530

    fixed the division logic

--- Calculator.py
+++ Calculator.py
@@ -2,7 +2,7 @@
 try:
     num1 = int(input('Enter the first number'))
     num2 = int(input('Enter the second number'))
-except Exception as e:
+except ValueError as e:
     print('please enter valid data')
     print(e)
     exit(0)
@@ -17,20 +17,20 @@ n = input("enter the operation + - * /")
 
 match n:
     case '+':
-        print(f'the answer of addition is{num1 + num2}')
+        print(f'the answer of addition is:{num1 + num2}')
 
     case '-':
-        print(f'the answer of addition is{num1 - num2}')
+        print(f'the answer of addition is:{num1 - num2}')
 
     case '*':
-        print(f'the answer of addition is{num1 * num2}')
+        print(f'the answer of addition is:{num1 * num2}')
 
     case '/':
 
         if num2 == 0:
             print('cannot divide by zero')
         else:    
-            print(f'the answer of addition is{num1 + num2}')    
+            print(f'the answer of addition is:{num1 / num2}')    
 
     case _:
         print('not a valid operator!')
